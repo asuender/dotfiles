fe7c6235ee2e75a180f9358ea5844570c40f03a4
Andreas Sünder <andreas.suender@icloud.com>
2026-08-08 14:26:50 +0200

    improve nvim config

--- .config/nvim/lua/plugins/core.lua
+++ .config/nvim/lua/plugins/core.lua
@@ -54,113 +54,26 @@ return {
                 sources = {
                     files = { hidden = true },
                     grep = { hidden = true },
-                    explorer = { hidden = true },
-                },
-            },
-        },
-    },
-
-    {
-        "numToStr/Comment.nvim",
-        opts = {
-            -- add any options here
-        },
-    },
-
-    {
-        "stevearc/conform.nvim",
-        opts = {
-            formatters_by_ft = {
-                typescript = { "prettier" },
-                typescriptreact = { "prettier" },
-            },
-        },
-    },
-
-    {
-        "nickjvandyke/opencode.nvim",
-        version = "*", -- Latest stable release
-        dependencies = {
-            {
-                -- `snacks.nvim` integration is recommended, but optional
-                ---@module "snacks" <- Loads `snacks.nvim` types for configuration intellisense
-                "folke/snacks.nvim",
-                optional = true,
-                opts = {
-                    input = {}, -- Enhances `ask()`
-                    picker = { -- Enhances `select()`
-                        actions = {
-                            opencode_send = function(...)
-                                return require("opencode").snacks_picker_send(
-                                    ...
-                                )
-                            end,
-                        },
-                        win = {
-                            input = {
-                                keys = {
-                                    ["<a-a>"] = {
-                                        "opencode_send",
-                                        mode = { "n", "i" },
-                                    },
-                                },
+                    explorer = {
+                        hidden = true,
+                        jump = { close = true },
+                        layout = {
+                            preset = "sidebar",
+                            preview = false,
+                            layout = {
+                                position = "float",
+                                col = 0,
+                                row = 0,
+                                width = 40,
+                                min_width = 40,
+                                height = 0, -- 0 = full height
+                                border = true,
                             },
                         },
                     },
                 },
             },
         },
-        config = function()
-            ---@type opencode.Opts
-            vim.g.opencode_opts = {
-                -- Your configuration, if any; goto definition on the type or field for details
-            }
-
-            vim.o.autoread = true -- Required for `opts.events.reload`
-
-            -- Recommended/example keymaps
-            vim.keymap.set({ "n", "x" }, "<C-a>", function()
-                require("opencode").ask("@this: ", { submit = true })
-            end, { desc = "Ask opencode…" })
-            vim.keymap.set({ "n", "x" }, "<C-x>", function()
-                require("opencode").select()
-            end, { desc = "Execute opencode action…" })
-            vim.keymap.set({ "n", "t" }, "<C-.>", function()
-                require("opencode").toggle()
-            end, { desc = "Toggle opencode" })
-
-            vim.keymap.set({ "n", "x" }, "go", function()
-                return require("opencode").operator("@this ")
-            end, { desc = "Add range to opencode", expr = true })
-            vim.keymap.set("n", "goo", function()
-                return require("opencode").operator("@this ") .. "_"
-            end, { desc = "Add line to opencode", expr = true })
-
-            vim.keymap.set("n", "<S-C-u>", function()
-                require("opencode").command("session.half.page.up")
-            end, { desc = "Scroll opencode up" })
-            vim.keymap.set("n", "<S-C-d>", function()
-                require("opencode").command("session.half.page.down")
-            end, { desc = "Scroll opencode down" })
-
-            -- You may want these if you use the opinionated `<C-a>` and `<C-x>` keymaps above — otherwise consider `<leader>o…` (and remove terminal mode from the `toggle` keymap)
-            vim.keymap.set(
-                "n",
-                "+",
-                "<C-a>",
-                { desc = "Increment under cursor", noremap = true }
-            )
-            vim.keymap.set(
-                "n",
-                "-",
-                "<C-x>",
-                { desc = "Decrement under cursor", noremap = true }
-            )
-        end,
-    },
-
-    {
-        "folke/snacks.nvim",
         keys = {
             {
                 "ff",
@@ -189,6 +102,35 @@ return {
                     Snacks.picker.grep_word()
                 end,
                 desc = "Search current word",
+            },
+        },
+    },
+
+    {
+        "numToStr/Comment.nvim",
+        opts = {
+            -- add any options here
+        },
+    },
+
+    {
+        "stevearc/conform.nvim",
+        opts = {
+            formatters_by_ft = {
+                typescript = { "prettier" },
+                typescriptreact = { "prettier" },
+            },
+        },
+    },
+
+    {
+        "nvim-lualine/lualine.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        opts = {
+            sections = {
+                lualine_x = { "location" },
+                lualine_y = {},
+                lualine_z = {},
             },
         },
     },
